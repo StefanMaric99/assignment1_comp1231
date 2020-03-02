1c86e9cab8d2bc701e0c54db8427630b1e34a9ff
StefanMaric99 <57594996+StefanMaric99@users.noreply.github.com>
2020-03-02 10:26:39 -0500

    Add files via upload

--- Comp1231_assignment1/101208175-functions.js
+++ Comp1231_assignment1/101208175-functions.js
@@ -1,38 +1,46 @@
 //Comp1231
 //Stefan Maric - 101208175
 //assignment 1
+//function1
 function stuInfo(nme, stdNum, nCourse, program, ptJob) {
+    if (ptJob === true){
+        console.log("My name is "+nme+", im in the "+program+" Program. I am taking "+nCourse+" this semester. I work part time.");
+        return "My name is "+nme+", im in the "+program+" Program. I am taking "+nCourse+" this semester. I work part time.";
 
-    nme = '';
-    stdNum = 0;
-    nCourse = 0;
-    program = '';
-
-    if (ptJob == true){
-        else console.log("My name is "+nme+", im in the "+program+" Program. I am taking "+nCourse+" this semester. I work part time.");
-
-    } else if (ptJob==false) {
-        return console.log("My name is "+nme+", im in the "+program+" Program. I am taking "+nCourse+" this semester. I do not work part time.");
-
+    } else if (ptJob===false) {
+        console.log("My name is "+nme+", im in the "+program+" Program. I am taking "+nCourse+" this semester. I do not work part time.");
     }
 
 }
-    stuInfo('stefan Maric',101208175,5, 't127',true);
 
+//function2
 function bDay_gradYear(a,graduateYear) {
     let  birthYear;
 
-    a = 0;
-    graduateYear = 0;
+
     birthYear = 2020 - a;
+    graduateYear = graduateYear+birthYear;
+
+    console.log("You were born in: "+birthYear + "\nYou will graduate in: "+graduateYear);
+}
+bDay_gradYear(0,7);
+//function3
+function tempCalc(cc,frn){
+    //0 celcius to ferenheit is 32F.
 
 
-    if(a==true){
-        return console.log("You were born in: "+birthYear);
-    }
-    if(graduateYear==true){
-        return console.log("You will graduate in: "+graduateYear);
-    }
+    console.log((cc * (9/5)) + 32);
+    console.log(frn-32);
+
 }
 
+function evenOrOdd(one){
 
+        for(let i =0;i<one;i++){
+            if(one=one/2){
+
+            } else if(){
+
+            }
+        }
+}
