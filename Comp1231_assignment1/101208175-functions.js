c97b8ca8a89e7c715246c589022e45261788334b
StefanMaric99 <57594996+StefanMaric99@users.noreply.github.com>
2020-03-05 23:05:42 -0500

    Add files via upload

--- Comp1231_assignment1/101208175-functions.js
+++ Comp1231_assignment1/101208175-functions.js
@@ -5,42 +5,88 @@
 function stuInfo(nme, stdNum, nCourse, program, ptJob) {
     if (ptJob === true){
         console.log("My name is "+nme+", im in the "+program+" Program. I am taking "+nCourse+" this semester. I work part time.");
-        return "My name is "+nme+", im in the "+program+" Program. I am taking "+nCourse+" this semester. I work part time.";
 
     } else if (ptJob===false) {
         console.log("My name is "+nme+", im in the "+program+" Program. I am taking "+nCourse+" this semester. I do not work part time.");
     }
 
 }
-
 //function2
 function bDay_gradYear(a,graduateYear) {
     let  birthYear;
 
-
-    birthYear = 2020 - a;
-    graduateYear = graduateYear+birthYear;
+    birthYear = 2020 + a;
+    graduateYear = graduateYear+2020;
 
     console.log("You were born in: "+birthYear + "\nYou will graduate in: "+graduateYear);
 }
-bDay_gradYear(0,7);
+
 //function3
 function tempCalc(cc,frn){
     //0 celcius to ferenheit is 32F.
-
 
     console.log((cc * (9/5)) + 32);
     console.log(frn-32);
 
 }
-
+//function4
 function evenOrOdd(one){
+    let evenCheck;
+    evenCheck = (one%2);
+    let trueNum,falseNum;
 
-        for(let i =0;i<one;i++){
-            if(one=one/2){
-
-            } else if(){
-
-            }
+    if(evenCheck==true){
+        for(let jj = 0;jj<=one;jj++){
+            trueNum += 1;
         }
+        console.log(trueNum);
+    } else {
+        console.log(falseNum-=1)
+    }
+
 }
+//function5
+function largestNum(n1,n2,n3) {
+    if(n1>n2&&n1>n3){
+        console.log("Largest int value is: "+n1);
+    } else if (n2>n1&&n2>n3){
+        console.log("Largest int value is: "+n2);
+    } else {
+        console.log("Largest int value is: "+n3);
+    }
+}
+//function6
+function valuesCheck(arrVal){
+    let tempArr = [12];
+    tempArr = arrVal;
+
+    let low,high;
+    tempArr.sort();
+    low = tempArr[0];
+    high = tempArr.length;
+
+}
+//function7
+function mCalc(firstN,secondN){
+    for(let k = 0;k<=secondN;k++){
+        let result = firstN*k;
+        console.log(firstN+" x "+k+" = "+result);
+    }
+}
+//function8
+function letterCase(letterCheck){
+    //
+    // if (character == character.toUpperCase()) {
+    //     console.log(character+" is upper case");
+    // }
+    // else if (character == character.toLowerCase()){
+    //     console.log(character+" is lower case");
+    // }
+    if(letterCheck>='a'&&letterCheck<='z'){
+        console.log(letterCheck+" is lower case");
+    } else {
+        console.log(letterCheck+" is upper case");
+    }
+
+}
+
